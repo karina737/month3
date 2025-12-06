0b2ff631129a6c0d42b9ea44c9e47b20ec968a64
Karina S <kakdelaa737@gmail.com>
2025-12-06 23:57:01 +0600

    Домашнее задание 4, задание 2

--- main.py
+++ main.py
@@ -7,23 +7,33 @@ def main(page : ft.Page):
     button_name=ft.Text(value = "Изменение темы :")
     page.theme_mode = ft.ThemeMode.LIGHT
     theme_status= ft.Text(value= 'Светлая')
-    
+    greeting_history=[]
+    history_text=ft.Text(value ='История приветствий :')
+     
     # greeting_text.value= 'Привет'
     # greeting_text.color= ft.Colors.GREEN
 
     def on_button_click(_):
         # print(name_input.value)
+
         name=name_input.value.strip()
 
         timestamp=datetime.now().strftime("%Y:%m:%d - %H:%M:%S")
 
         if name:
-            greeting_text.value = f' {timestamp} - Hello {name}'
+            greeting_text.value = None
             greeting_text.color=None
+            greeting_text.spans= ft.TextSpan(f"{timestamp} - Hello "),ft.TextSpan(name,style=ft.TextStyle(weight=ft.FontWeight.BOLD,color=ft.Colors.BLUE  ))
             name_input.value=None
+            
+            greeting_history.append(f' {timestamp} - {name}')
+            print(greeting_history)
+            history_text.value='История приветсвий : \n' + '\n'.join(greeting_history)
+                
         else:
             greeting_text.value= 'Введите корректное имя'
             greeting_text.color=ft.Colors.RED
+            greeting_text.spans=None
 
         page.update()
 
@@ -37,16 +47,27 @@ def main(page : ft.Page):
 
         page.update()
 
-    name_input= ft.TextField(label ='Введите имя', on_submit= on_button_click)
-
+    name_input= ft.TextField(label ='Введите имя', on_submit= on_button_click, expand=True)
+       
     button_text=ft.TextButton(text='send',on_click=on_button_click)
     button_elevated=ft.ElevatedButton(text='send',on_click=on_button_click)
     button_icon_send=ft.IconButton(ft.Icons.SEND, on_click=on_button_click)
     button_icon_sun=ft.IconButton(ft.Icons.BRIGHTNESS_7, on_click=button_sun)
+    
 
+    def clear_history(_):
+        print(greeting_history)
+        greeting_history.clear()
+        history_text.value = 'История приветствий :'
+        page.update()
+        print(greeting_history)
+    clear_button=ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history)
 
-    page.add(greeting_text, name_input,button_text, button_elevated,button_icon_send)
-    page.add(ft.Row([button_icon_sun,button_name ,theme_status]))
+    
+    view_greeting_text=ft.Row([greeting_text],alignment=ft.MainAxisAlignment.CENTER)
+    page.add(view_greeting_text, ft.Row([name_input,button_icon_send, clear_button]), history_text)
+    
+    # page.add(ft.Row([button_icon_sun,button_name ,theme_status]))
 
 ft.app(target=main)
 
